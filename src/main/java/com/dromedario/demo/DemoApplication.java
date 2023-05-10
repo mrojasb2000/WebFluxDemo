b0bff23253bdf6a29e8f8eb13a73c0f3d2dd9db9
Mauricio Rojas Barrientos <maalroja@bancolombia.com.co>
2023-05-10 18:51:11 -0400

    chore: add basic flux operator

--- src/main/java/com/dromedario/demo/DemoApplication.java
+++ src/main/java/com/dromedario/demo/DemoApplication.java
@@ -1,13 +1,27 @@
 package com.dromedario.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import reactor.core.publisher.Flux;
+
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	public void run(String... args) throws Exception {
+		System.out.println("Before callback");
+		intervalOperatorFromCreateExample();
+		System.out.println("After callback");
+	}
+
+	private void intervalOperatorFromCreateExample() {
+		Flux.create(emitter -> {
+			System.out.println("Hello from emitter");
+		});
+	}
 }
